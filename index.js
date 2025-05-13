4a7ee16e0c613baad8443cd03f832e42ba1d15d0
mohamed7855 <martm7726@gmail.com>
2025-05-13 21:07:33 +0300

    OOP

---
+++ index.js
@@ -0,0 +1,21 @@
+class human {
+  #name;
+  constructor(name, age) {
+    this.#name = name;
+    this.age = age;
+  }
+
+  setName(newName){
+    this.#name= newName
+  }
+  getName(){
+    console.log(this.#name);
+  }
+
+}
+
+let ali = new human("ali", 50);
+console.log(ali);
+ali.setName("khaled")
+console.log(ali);
+ali.getName()
